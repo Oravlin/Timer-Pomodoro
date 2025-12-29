2e525b7dd97e952724fdbfc6b8a53ea6195c86f9
Alvaro <alvaroandradesantos2009@gmail.com>
2025-12-29 19:12:34 -0300

    5 - tentativa do java script

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,16 @@
 let tempo = 1500; //coloquei o tempo em segundos pq é mais facíl :)
 let minutosTimer = 0;
 let segundosTimer = 0;
-let timer
+let timer;
+let pausado = true;
+
+function pausarPomo() {
+    pausado = true;
+}
+
+function iniciarPomo() {
+    pausado = false;
+}
 
 function formatarTempo(tempo) {
     let min = Math.floor(tempo / 60);
@@ -11,25 +20,60 @@ function formatarTempo(tempo) {
 }
 
 function iniciarTimerFoco() {
-    clearInterval(timer);
     timer = setInterval(() => {
-        tempo--;
-        document.getElementById('timerText').textContent = formatarTempo(tempo);
+        if (tempo > 0) {
+            tempo--;
+            document.getElementById('timerText').textContent = formatarTempo(tempo);
+        }
+        else {
+            clearInterval(timer);
+            tempo = 300;
+            iniciarTimerPausa();
+        }
     }, 1000);
 }
 function iniciarTimerPausa() {
     clearInterval(timer);
     timer = setInterval(() => {
-        tempo--;
-        document.getElementById('timerText').textContent = formatarTempo(tempo)
-    }, 1000)
+        if (tempo > 0) {
+            tempo--;
+            document.getElementById('timerText').textContent = formatarTempo(tempo);
+        }
+        else {
+            clearInterval(timer);
+        }
+        if(pausado){
+            Pomodoro();
+        }
+    }, 1000);
+    return tempo = 'oi';
 }
 
-function comecarPomodoro() {
-    tempo = 1500;
-    iniciarTimerFoco();
-    if (tempo == 0) {
-        tempo = 300;
-        iniciarTimerPausa();
+function Pomodoro() {
+    if (pausado) {
+        pausado = false;
+        let i = 0;
+        tempo = 10;
+        iniciarTimerFoco();
+        let intervaloPomodoro = setInterval(() => {
+            if (i < 4) {
+                if (tempo == 'oi') {
+                    tempo = 1500;
+                    iniciarTimerFoco();
+                    i++
+                }
+            }
+            else {
+                clearInterval(intervaloPomodoro);
+            }
+        }, 1000);
     }
+}
+
+function resetTimer(){
+    tempo = 1500;
+    clearInterval(intervaloPomodoro);
+    clearInterval(timer);
+    document.getElementById('timerText').textContent = formatarTempo(tempo);
+    pausado = true;
 }
