5624bbbebf13d3fefed144fbf49805a29ba63248
Alvaro <alvaroandradesantos2009@gmail.com>
2025-12-30 01:04:29 -0300

    7 - ajustes finos ao js

--- src/js/script.js
+++ src/js/script.js
@@ -9,47 +9,77 @@ function formatarTempo(t) {
 
 function timerFoco() {
     return new Promise((resolve) => {
-        tempo = 10;
-        document.getElementById('timerText').textContent = formatarTempo(tempo);
-        foco = setInterval(() => {
-            if (tempo > 0) {
-                tempo--;
-                document.getElementById('timerText').textContent = formatarTempo(tempo);
-            }
-            else {
-                clearInterval(foco);
-                resolve();
-            }
-        }, 1000);
+        if (pausado == false) {
+            document.getElementById('timerText').textContent = formatarTempo(tempo);
+            foco = setInterval(() => {
+                if (tempo > 0) {
+                    tempo--;
+                    document.getElementById('timerText').textContent = formatarTempo(tempo);
+                }
+                else {
+                    clearInterval(foco);
+                    resolve();
+                }
+            }, 1000);
+        }
+        else {
+            resolve()
+            return;
+        }
     })
 }
 
 function timerPausa() {
     return new Promise((resolve) => {
-        tempo = 10;
-        document.getElementById('timerText').textContent = formatarTempo(tempo);
-        pausa = setInterval(() => {
-            if (tempo > 0) {
-                tempo--;
-                document.getElementById('timerText').textContent = formatarTempo(tempo);
-            }
-            else {
-                clearInterval(pausa);
-                resolve();
-            }
-        }, 1000);
+        if (pausado == false) {
+            document.getElementById('timerText').textContent = formatarTempo(tempo);
+            pausa = setInterval(() => {
+                if (tempo > 0) {
+                    tempo--;
+                    document.getElementById('timerText').textContent = formatarTempo(tempo);
+                }
+                else {
+                    clearInterval(pausa);
+                    resolve();
+                }
+            }, 1000);
+        }
+        else {
+            resolve()
+            return;
+        }
     })
+}
+
+async function resetPomodoro() {
+    document.getElementById('btn-play').classList.remove('hide')
+    document.getElementById('btn-pause').classList.add('hide');
+    document.getElementById('btn-reset').classList.add('hide');
+    pausado = true;
+    tempo = 1500;
+    i = 0;
+    document.getElementById('timerText').textContent = formatarTempo(tempo);
+    clearInterval(foco);
+    clearInterval(pausa);
 }
 
 async function pomodoro() {
     if (pausado) {
+        document.getElementById('btn-play').classList.add('hide');
+        document.getElementById('btn-reset').classList.remove('hide')
+        document.getElementById('btn-pause').classList.remove('hide')
         pausado = false;
         let i = 0;
-        while (i < 4) {
+        while (i < 4 && pausado == false) {
+            tempo = 1500;
             await timerFoco();
+            tempo = 300;
             await timerPausa();
             i++
         }
         pausado = true;
+        document.getElementById('btn-play').classList.remove('hide');
+        document.getElementById('btn-pause').classList.add('hide');
+        document.getElementById('btn-reset').classList.add('hide');
     }
 }
