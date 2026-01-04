3cd83462143d17fa5ad56cc743ddf36e4c8d86cf
Alvaro <alvaroandradesantos2009@gmail.com>
2026-01-04 15:12:45 -0300

    8 - polimento do js e adicionando função de pausar

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,8 @@
-let tempo //esse mano vai ser em segundos
+let tempo = 1500; //esse mano vai ser em segundos
 let pausado = true;
+let tempoFoco = 1500;
+let tempoPausa = 300;
+let modoFoco = true;
 
 function formatarTempo(t) {
     let min = Math.floor(t / 60);
@@ -7,79 +10,54 @@ function formatarTempo(t) {
     return `${min}:${seg < 10 ? '0' : ''}${seg}`;
 }
 
-function timerFoco() {
-    return new Promise((resolve) => {
-        if (pausado == false) {
-            document.getElementById('timerText').textContent = formatarTempo(tempo);
-            foco = setInterval(() => {
-                if (tempo > 0) {
-                    tempo--;
-                    document.getElementById('timerText').textContent = formatarTempo(tempo);
-                }
-                else {
-                    clearInterval(foco);
-                    resolve();
-                }
-            }, 1000);
-        }
-        else {
-            resolve()
-            return;
-        }
-    })
+function alternarCiclo(){
+    modoFoco = !modoFoco;
+    tempo = modoFoco ? tempoFoco : tempoPausa;
+    document.getElementById('timerText').textContent = formatarTempo(tempo);
+    timer();
 }
 
-function timerPausa() {
-    return new Promise((resolve) => {
-        if (pausado == false) {
+function timer() {
+    foco = setInterval(() =>{
+        if (tempo > 0){
+            tempo--;
             document.getElementById('timerText').textContent = formatarTempo(tempo);
-            pausa = setInterval(() => {
-                if (tempo > 0) {
-                    tempo--;
-                    document.getElementById('timerText').textContent = formatarTempo(tempo);
-                }
-                else {
-                    clearInterval(pausa);
-                    resolve();
-                }
-            }, 1000);
         }
-        else {
-            resolve()
-            return;
+        else{
+            clearInterval(foco);
+            alternarCiclo();
         }
-    })
+    }, 1000)
 }
+
+async function pausarPomodoro(){
+    if(pausado == false){
+        clearInterval(foco);
+        pausado = true;
+        document.getElementById('btn-play').classList.remove('hide');
+        document.getElementById('btn-pause').classList.add('hide');
+    }
+
+}
+
 
 async function resetPomodoro() {
     document.getElementById('btn-play').classList.remove('hide')
     document.getElementById('btn-pause').classList.add('hide');
     document.getElementById('btn-reset').classList.add('hide');
     pausado = true;
+    modoFoco = true;
     tempo = 1500;
-    i = 0;
     document.getElementById('timerText').textContent = formatarTempo(tempo);
     clearInterval(foco);
-    clearInterval(pausa);
 }
 
 async function pomodoro() {
     if (pausado) {
-        document.getElementById('btn-play').classList.add('hide');
-        document.getElementById('btn-reset').classList.remove('hide')
-        document.getElementById('btn-pause').classList.remove('hide')
         pausado = false;
-        let i = 0;
-        while (i < 4 && pausado == false) {
-            tempo = 1500;
-            await timerFoco();
-            tempo = 300;
-            await timerPausa();
-            i++
-        }
-        pausado = true;
-        document.getElementById('btn-play').classList.remove('hide');
-        document.getElementById('btn-pause').classList.add('hide');
-        document.getElementById('btn-reset').classList.add('hide');
+        document.getElementById('btn-play').classList.add('hide');
+        document.getElementById('btn-pause').classList.remove('hide');
+        document.getElementById('btn-reset').classList.remove('hide');
+        timer();
     }
 }
